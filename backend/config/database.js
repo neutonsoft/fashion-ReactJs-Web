94d7e03de2c0972667718b62ca8f22517b208e0e
Pratius Kumar Dubey <pratius.vin@gmail.com>
2022-10-22 12:10:21 +0400

    Added URl

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URL;
 console.log("MONGO_URI==",MONGO_URI)
 const connectDatabase = () => {
     mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
